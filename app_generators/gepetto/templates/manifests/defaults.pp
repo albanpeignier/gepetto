bf8095c52be681cec766d880b068b70976e683d7
Alban Peignier <alban.peignier@free.fr>
2009-12-15 09:11:26 +0100

    Ignore ~ files

--- app_generators/gepetto/templates/manifests/defaults.pp
+++ app_generators/gepetto/templates/manifests/defaults.pp
@@ -3,7 +3,7 @@
 Exec { path => "/usr/bin:/usr/sbin/:/bin:/sbin:/usr/local/bin:/usr/local/sbin" }
 
 File { 
-  ignore => ['.svn', '.git', 'CVS' ], 
+  ignore => ['.svn', '.git', 'CVS', '*~' ], 
   checksum => md5, 
   owner => root, group => root, 
   backup => server 
